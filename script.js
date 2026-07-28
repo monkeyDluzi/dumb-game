e05b60976b51c463005b3123f5539be5649a3491
lousainachicken <Luziandre588@gmail.com>
2026-07-27 23:15:52 -0400

    Add cookie click functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let cookies = 0;
 
+const cookie = document.getElementById("cookie");
+const score = document.getElementById("score");
+
+cookie.addEventListener("click", function () {
+
+    cookies++;
+
+    score.textContent = "Cookies: " + cookies;
+
+});
